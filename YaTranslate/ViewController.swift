c208af82ffd461d21c862fcc672392d245d4f73b
Admin <admin@mac-admin.local>
2019-06-10 06:53:50 +0300

    Initial Commit

---
+++ YaTranslate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YaTranslate
+//
+//  Created by Admin on 10.06.2019.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
